60b4f85f185a15d8212d3859ccd5fad54262841f
Yaks Souza <pl4nk1ton@gmail.com>
2021-08-02 19:34:19 -0300

    Fix http name

--- api/index.go
+++ api/index.go
@@ -1,10 +1,10 @@
 package handler
 import (
-	api "github.com/Plankiton/PraAmar-back"
+	"github.com/Plankiton/PraAmar-back"
 	"net/http"
 )
 
-var handler Http.Handler = api.Setup()
+var handler http.Handler = api.Setup()
 func Handler(w http.ResponseWriter, r *http.Request) {
 	handler.ServeHTTP(w, r)
 }
